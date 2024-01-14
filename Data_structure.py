2ff32e94d25905bfacd096b463a42975926074fc
Vova deduskin lap <vladimir.sehirev@gmail.com>
2024-01-14 18:37:11 +0200

    before tallinn

--- Data_structure.py
+++ Data_structure.py
@@ -32,22 +32,65 @@
 
 #par checker
 
-input_str = "((()))"
+# input_str = "[]()[(]()))"
+#
+# def par_checker():          #using list
+#     stack = []
+#     for char in input_str:
+#         if char == "(" or char == "[":
+#             stack.append(char)
+#         elif char == ")" or char == "]":
+#             if len(stack) > 0 and (stack[-1] == "(" or stack[-1] == "["):
+#                 stack.pop()
+#         # else:
+#         #     return False
+#
+#     if len(stack) == 0:
+#         print("balanced")
+#     else:
+#         print("not balanced")
+#
+# par_checker()
 
-def par_checker():
-    stack = []
-    for char in input_str:
-        if char == "(":
-            stack.append(char)
-        elif char == ")":
-            if len(stack) > 0 and stack[-1] == "(":
-                stack.pop()
-        # else:
-        #     return False
 
-    if len(stack) == 0:
-        print(True)
-    else:
-        print(False)
+# input_str = "(())[]"
+#
+# sym_dict = {")": "(", "]": "["}
+# def par_checker():          #using dictionary
+#     stack = []
+#     for char in input_str:
+#         if char in sym_dict.values():
+#             stack.append(char)
+#         elif char in sym_dict.keys():
+#             if len(stack) > 0 and stack[-1] == sym_dict[char]:
+#                 stack.pop()
+#             else:
+#                 return False
+#
+#
+#     if len(stack) == 0:
+#         print("balanced")
+#     else:
+#         print("not balanced")
+#
+# par_checker()
 
-par_checker()
+# Grahps
+
+# Alice (Person 1)
+# Bob (Person 2)
+# Carol (Person 3)
+# Dave (Person 4)
+
+# Alice is friends with Bob and Carol.
+# Bob is friends with Carol.
+# Carol is friends with Dave.
+# Dave is friends with Alice.
+
+social_network = {
+    1: [2, 3],    # Alice is friends with Bob (2) and Carol (3)
+    2: [3],      # Bob is friends with Carol (3)
+    3: [4],      # Carol is friends with Dave (4)
+    4: [1]       # Dave is friends with Alice (1)
+}
+
