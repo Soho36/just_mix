6e396c06346b473b69349d09b6bd9477dfc9e355
Vova deduskin lap <vladimir.sehirev@gmail.com>
2024-01-14 14:21:05 +0200

    queue silla

--- queue.py
+++ queue.py
@@ -6,18 +6,21 @@ class Queue:
     def queue_max_size_reached(self):
         if len(self.items) == self.max_size:
             return True
+
     def is_empty(self):
         return len(self.items) == 0
 
     def enqueue(self, item):
         if not self.queue_max_size_reached():
             self.items.append(item)
+            print("new element was successfully enqueued")
         else:
             print("que max size is reached")
 
     def dequeue(self):
         if not self.is_empty():
-            return self.items.pop(0) #returns and removes first element
+            print("first element was successfully dequeued")
+            return self.items.pop(0)  # returns and removes first element
         else:
             print("queue is empty")
 
@@ -28,33 +31,59 @@ class Queue:
         else:
             print("queue is empty")
 
+    def show_full_queue(self):
+        if not self.is_empty():
+            print("full que: ", self.items)
+        else:
+            print("queue is empty")
+
     def size(self):
-        print("Queue size is: ", len(self.items))
+        print("queue size is: ", len(self.items))
         return len(self.items)
 
-#user cmds
+# user cmds
 # add_to_queue = "add"
 # remove_from_queue = "remove" #returns and removes first element
 # peek_first_element = "show first"
 # show_queue_size = "show size"
 
-queue_max_size = int(input("Enter the Queue max size: "))
-queue1 = Queue(queue_max_size)
 
-while True:
-    user_input = str(input("add, remove, show, size: "))
-    if user_input == "add":
+def user_interface():
+    queue_max_size = int(input("Enter the Queue max size: "))
+    queue1 = Queue(queue_max_size)
+    while True:
+        user_cmd = str(input("add, remove, show first, show queue, size, exit: "))
+        if user_cmd == "add":
+            user_input = input("enter element to enqueue: ")
+            queue1.enqueue(user_input)
 
-        queue1.enqueue(user_input)
+        elif user_cmd == "remove":
+            queue1.dequeue()
+
+        elif user_cmd == "show first":
+            queue1.peek()
+
+        elif user_cmd == "size":
+            queue1.size()
+
+        elif user_cmd == "exit":
+            print("program stopped")
+            exit()
+
+        elif user_cmd == "show queue":
+            queue1.show_full_queue()
+
+        else:
+            print("wrong input")
 
 
+if __name__ == "__main__":
+    user_interface()
 
-#init
 
+# init
 
-# queue1.enqueue([1, {"name": "Covertex"}, 6])
+# queue1.enqueue([1, {"name": "dd"}, 6])
 # queue1.enqueue(63)
 # queue1.enqueue(98.5)
 # queue1.enqueue("chart")
-
-
