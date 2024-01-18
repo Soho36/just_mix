ee10725abce0167602e822baea64539738533394
Soho36 <vladimir.sehirev@gmail.com>
2024-01-19 00:58:06 +0200

    binary tree

---
+++ Binary tree.py
@@ -0,0 +1,66 @@
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left_node = None
+        self.right_node = None
+
+    def insert_new_left_node(self, new_value):
+        if self.left_node is None:
+            self.left_node = new_value
+
+        else:
+            new_node = new_value    #new node will get the new value
+            new_node.left_node = self.left_node #left node of the old node becomes left node of the new node
+            self.left_node = new_node #left node of the current node is updated to the new node
+        return self
+
+
+    def insert_new_right_node(self, new_value):
+        if self.right_node is None:
+            self.right_node = new_value
+
+        else:
+            new_node = new_value
+            new_node.right_node = self.right_node
+            self.right_node = new_node
+        return self
+
+    def display_hierarchy(self, level=0):
+        indentation = "    " * level
+        print(f"{indentation}{self.value}")
+        if self.left_node:
+            self.left_node.display_hierarchy(level + 1)
+        if self.right_node:
+            self.right_node.display_hierarchy(level + 1)
+
+node_1 = BinaryTree("2 root")
+node_2 = BinaryTree("7 left")
+node_3 = BinaryTree("5 right")
+node_4 = BinaryTree("2 left")
+node_5 = BinaryTree("6 right")
+node_6 = BinaryTree("9 right")
+node_7 = BinaryTree("5 left")
+node_8 = BinaryTree("11 right")
+node_9 = BinaryTree("4 left")
+
+node_1.insert_new_left_node(node_2)
+node_1.insert_new_right_node(node_3)
+node_2.insert_new_right_node(node_5)
+node_2.insert_new_right_node(node_4)
+node_5.insert_new_left_node(node_7)
+node_5.insert_new_right_node(node_8)
+node_3.insert_new_left_node(node_6)
+node_3.insert_new_left_node(node_9)
+
+node_1.display_hierarchy()
+
+
+
+
+
+
+
+
+
+
+
